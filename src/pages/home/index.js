9724424eee07d2f4539c9dacd490bb9255971762
Nijoane <nicajoane@outlook.com>
2021-02-20 14:23:27 -0300

    [style] finalizando responsividade da home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,14 +3,13 @@ export const Home = () => {
   const rootElement = document.createElement('div');
   const signupHtml = `
     <main>
-      <section class="container-home">
+      <section class="welcome-home">
         <h1>Welcome!</h1>
-        <p>Connect with people from all over the world and learn to appreciate the natural beauty.</p>
         <img src="images/welcome.png" alt="Welcome to BeeU">
-        <section class="btn-container">
-          <button class="btn-login" id="btn-login">Login</button> 
-          <button class="btn-signup" id="btn-signup">SignUp</button>
-        </section> 
+        <p>Connect with people from all over the world and learn to appreciate the natural beauty.</p>
+
+        <button class="login" id="btn-login">Login</button> 
+        <button class="signup" id="btn-signup">SignUp</button>
       </section>
     </main>`;
   rootElement.innerHTML = signupHtml;

0a562a25137ed05fce5c695c777346baa2fb75cd
Carol Shimada <caroline_shimada@hotmail.com>
2021-02-22 14:12:57 -0300

    Like funcionando porem precisa atualizar a pagina pra mostrar atualizado

--- src/components/postComponent/post.js
+++ src/components/postComponent/post.js
@@ -1,25 +1,55 @@
-/* eslint-disable no-console */
+import { onNavigate } from '../../utils/history.js';
+
+export const updatelikePost = async (postId) => {
+  const numberOfLikesElement = document.querySelector(`#likes-counter-${postId}`);
+  // eslint-disable-next-line no-unused-vars
+  const numberOfLikes = Number(numberOfLikesElement.textContent);
+  await firebase.firestore().collection('posts').doc(postId).get()
+    .then((doc) => {
+      if (doc.data().likes === 0) {
+        firebase.firestore().collection('posts').doc(postId).update({
+          likes: firebase.firestore.FieldValue.increment(1),
+        });
+        // numberOfLikesElement.textContent = numberOfLikes + 1;
+      } else {
+        firebase.firestore().collection('posts').doc(postId).update({
+          likes: firebase.firestore.FieldValue.increment(-1),
+        });
+        // numberOfLikesElement.textContent = numberOfLikes - 1;
+      }
+    });
+};
+export const addLikeListener = (post) => {
+  const postId = document.getElementById(post.id);
+  // eslint-disable-next-line no-unused-vars
+  postId.addEventListener('click', (e) => {
+    updatelikePost(post.id);
+    onNavigate('/feed');
+  });
+};
 function renderPost(user) {
   const postContainer = document.querySelector('.posted-text');
   firebase.firestore().collection('posts').where('user', '==', user.uid).get()
     .then((querySnapshot) => {
       querySnapshot.forEach((post) => {
         const database = post.data();
-        const postElement = `
-          <ul>
-            <li>${database.name}</li>
-            <li>
-              <img src=${database.image} alt="Image">
-            <li>
-            <li>${database.date} ${database.time}</li>
-            <li>${database.text}</li>
-            <li>${database.likes}</li>
-            <li>
-              <input class="comment" placeholder="Comment" type="text">
-              <button class="btn-comment">Send</button>
-            </li>
-          </ul>`;
-        postContainer.innerHTML += postElement;
+        const postElement = document.createElement('div');
+        postElement.innerHTML = `
+            <li class="name-profile">${database.name}</li>
+            <li class="date-post">${database.date} ${database.time}</li>
+            <li class="post-posted">${database.text}</li>
+            <img class="profile-pic" src=${database.image} alt="Image">
+            <div class="vertical-infos-post">
+            
+            <div class="buttons-social">
+              <a id='likes-counter-${post.id}'>${database.likes}</a> 
+              <button id='${post.id}' class='btn-like'><i>👍</i></button>
+              <button class='btn-coment'><i>💬</button>
+            </div>
+            <input class="comment" placeholder="Comment" type="text">
+            `;
+        postContainer.append(postElement);
+        addLikeListener(post);
       });
     })
     .catch(() => {
@@ -28,7 +58,7 @@ function renderPost(user) {
       postContainer.innerHTML = postElement;
     });
 }
-
+export default renderPost;
 export const showPosts = () => firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     renderPost(user);

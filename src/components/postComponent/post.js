f0732ac32a3ab9b6d6c181ee12a878a2110ebcca
Nijoane <nicajoane@outlook.com>
2021-04-19 21:48:06 -0300

    adicionando icones

--- src/components/postComponent/post.js
+++ src/components/postComponent/post.js
@@ -1,16 +1,18 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-unused-vars */
 import { onNavigate } from '../../utils/history.js';
 
-var clicked = false;
+let clicked = false;
 
 export const updatelikePost = async (postId, postElement) => {
-  if(!clicked){
+  if (!clicked) {
     clicked = true;
     const numberOfLikesElement = postElement.querySelector(`#likes-counter-${postId}`);
     const numberOfLikes = Number(numberOfLikesElement.textContent);
     await firebase.firestore().collection('posts').doc(postId).get()
       .then((doc) => {
         if (doc.data().likes === 0) {
-          firebase.firestore().collection('posts').doc(postId).update({ 
+          firebase.firestore().collection('posts').doc(postId).update({
             likes: firebase.firestore.FieldValue.increment(1),
           });
           numberOfLikesElement.textContent = numberOfLikes + 1;
@@ -20,9 +22,9 @@ export const updatelikePost = async (postId, postElement) => {
           });
           numberOfLikesElement.textContent = numberOfLikes - 1;
         }
-    });
-    
-    setTimeout(function(){
+      });
+
+    setTimeout(() => {
       clicked = false;
     }, 1000);
   }
@@ -31,36 +33,35 @@ export const updatelikePost = async (postId, postElement) => {
 export const addLikeListener = (post, postElement) => {
   const postId = document.getElementById(post.id);
   postId.addEventListener('click', (e) => {
-    e.preventDefault;
+    e.preventDefault();
     updatelikePost(post.id, postElement);
   });
-}
+};
 
 export const deletePost = (postId) => {
   firebase.firestore()
-  .collection('posts')
-  .doc(postId)
-  .delete();
-}
+    .collection('posts')
+    .doc(postId)
+    .delete();
+};
 
 export const updateTextPost = (postId, newText) => {
-    firebase.firestore().collection('posts').doc(postId).update({
-      text: newText
-    });
-}
+  firebase.firestore().collection('posts').doc(postId).update({
+    text: newText,
+  });
+};
 
 export const addUpdateTextListener = (post, postElement) => {
-  postElement.querySelector('.button-update-' + post.id).addEventListener('click', (e) => {
+  postElement.querySelector(`.button-update-${post.id}`).addEventListener('click', (e) => {
     e.preventDefault();
-    const updateText = document.getElementById('update-text-' + post.id).value;
-    console.log(updateText);
+    const updateText = document.getElementById(`update-text-${post.id}`).value;
     updateTextPost(post.id, updateText);
     onNavigate('/feed');
   });
-}
+};
 
 export const addDeleteListener = (post, postElement) => {
-  postElement.querySelector('.button-delete-' + post.id).addEventListener('click', (e) => {
+  postElement.querySelector(`.button-delete-${post.id}`).addEventListener('click', (e) => {
     e.preventDefault();
     const confirmDelete = window.confirm('Deseja deletar o post?');
     if (confirmDelete === true) {
@@ -69,15 +70,13 @@ export const addDeleteListener = (post, postElement) => {
       onNavigate('/feed');
     }
   });
-}
-
-export const updateComments = (id, subComment) => {
-  return firebase
-    .firestore()
-    .collection('post')
-    .doc(id)
-    .update({ comments: firebase.firestore.FieldValue.arrayUnion(subComment) });
 };
+
+export const updateComments = (id, subComment) => firebase
+  .firestore()
+  .collection('post')
+  .doc(id)
+  .update({ comments: firebase.firestore.FieldValue.arrayUnion(subComment) });
 
 function compare(a, b) {
   if (a.time < b.time) {
@@ -90,19 +89,18 @@ function compare(a, b) {
   return 0;
 }
 
-
 function renderPost(user) {
   const postContainer = document.querySelector('.posted-text');
   firebase.firestore().collection('posts')
-  .where('user', '==', user.uid)
-  .get()
+    .where('user', '==', user.uid)
+    .get()
     .then((querySnapshot) => {
       querySnapshot.forEach((post) => {
-
         const database = post.data();
         const postElement = document.createElement('div');
         postElement.classList.add('post-individual');
-        var data = new Date(database.time)
+        const data = new Date(database.time);
+
         postElement.innerHTML = `
         <div class="profile-items">
           <div class="up-profile">  
@@ -116,19 +114,24 @@ function renderPost(user) {
             <div class="vertical-infos-post">
             <div class="buttons-social">
               <a class="likes-counters" id='likes-counter-${post.id}'>${database.likes}</a> 
-              <button id='${post.id}' class='btn-like'>💛</button>
-              <button id="button-delete"class='button-delete-${post.id}' data-delete='${post.id}'>🗑️</button>
+              <button id='${post.id}' class='btn-like'>
+                <img src="/images/favorite.svg" class="favorite-icon">
+              </button>
+              <button id="button-delete"class='button-delete-${post.id}' data-delete='${post.id}'>
+                <img src="/images/trash.svg" class="delete-icon">
+              </button>
             <div>
               <div id="modal-promocao" class="modal-container> 
                 <div class="modal">
                   <form class="update-edit-post">
                     <input class="update-texts"id="update-text-${post.id}" placeholder="Edit your post here" type="text">
-                    <button id="button-edit"class='button-update-${post.id}' <i>✓</button>
+                    <button id="button-edit"class='button-update-${post.id}' 
+                      <i><img src="/images/done.svg" class="done-icon">
+                    </button>
                   </form>
               </div>
               </div>
             `;
-            
         postContainer.append(postElement);
         addLikeListener(post, postElement);
         addDeleteListener(post, postElement);
@@ -149,4 +152,3 @@ export const showPosts = () => firebase.auth().onAuthStateChanged((user) => {
   }
   return null;
 });
-
